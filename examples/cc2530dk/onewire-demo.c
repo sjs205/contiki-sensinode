342021070574b802e672c5710fb8e8b948e02664
sjs205 <swannonline@googlemail.com>
2016-12-13 22:35:55 +0000

    Add DS18B20 Temp sensor onewire device

--- examples/cc2530dk/onewire-demo.c
+++ examples/cc2530dk/onewire-demo.c
@@ -81,23 +81,58 @@
 PROCESS(onewire_test_process, "Onewire Test Process");
 AUTOSTART_PROCESSES(&onewire_test_process);
 /*---------------------------------------------------------------------------*/
+static void convert_temp_str(int16_t temp_2c, char *temp_s);
+static float convert_temp_float(int16_t temp_2c);
+static void temp_conversion_tests();
+static void print_rom_id(struct onewire_dev *ow);
+static void print_scratch_pad(const char *buf, uint8_t count);
+/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(onewire_test_process, ev, data)
 {
   static struct etimer et;
 
   static struct onewire_dev ow;
   static struct search_rom srom;
-  unsigned buf[10];
-  /*ow.id = 0x0;
-  ow.family = 0x0;
-  ow.crc = 0x0;
-*/
+  uint8_t buf[10];
+  int16_t temp = 0;
+  char temp_str[10];
+  int neg = 0;
+  float sane = 0;
+  int dec;
+  float frac;
 
   PROCESS_BEGIN();
 
   PRINTF("=========================\n");
   PRINTF("Starting Onewire Example.\n");
   PRINTF("=========================\n");
+
+  PRINTF("Temp conversion tests:\n");
+  temp_conversion_tests();
+
+  PRINTF("===============\n");
+  PRINTF("TESTING ONEWIRE\n");
+  PRINTF("===============\n");
+
+#if SINGLE_DROP
+  /* read rom address */
+  if (onewire_read_rom(&ow)) {
+    print_rom_id(&ow);
+  } else {
+    PRINTF("ERROR: Failed to read ROM id\n");
+    continue;
+  }
+#else
+  /* prepare for rom search */
+  onewire_search_rom_setup(&srom, &ow);
+  PRINTF("Performing ROM search:\n");
+  if (onewire_search_rom(&srom, &ow)) {
+    PRINTF("ROM search sucessful\n");
+    print_rom_id(&ow);
+  } else {
+    PRINTF("ERROR: ROM search failed\n");
+  }
+#endif
 
   /* Set an etimer. We take sensor readings when it expires and reset it. */
   etimer_set(&et, CLOCK_SECOND * 2);
@@ -107,56 +142,6 @@ PROCESS_THREAD(onewire_test_process, ev, data)
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
     PRINTF("===============\n");
-    PRINTF("TESTING ONEWIRE\n");
-    PRINTF("===============\n");
-    onewire_read_rom(&ow);
-    PRINTF("Family: 0x%X\n", ow.family);
-    PRINTF("ID: 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X\n", ow.id[0],ow.id[1],
-        ow.id[2],ow.id[3], ow.id[4],ow.id[5]);
-    PRINTF("CRC: 0x%X\n", ow.crc);
-
-    if (onewire_skip_rom()) {
-      /* perform temp conversion */
-      onewire_write_cmd(0x44);
-    }
-
-    PRINTF("temp conversion started\n");
-
-    if (onewire_skip_rom()) {
-      PRINTF("Reading scratch pad:\n");
-      onewire_read_fn_cmd_bytes(0xBE, buf, 9);
-      PRINTF("0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X\n",
-          buf[0],buf[1], buf[2],buf[3], buf[4],buf[5],
-          buf[6],buf[7], buf[8]);
-    } else {
-      PRINTF("Failed to match ROM\n");
-    }
-    /* Could use a skip_rom */
-    if (onewire_match_rom(&ow)) {
-      PRINTF("MATCH: Reading scratch pad:\n");
-      onewire_read_fn_cmd_bytes(0xBE, buf, 9);
-      PRINTF("0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X\n",
-          buf[0],buf[1], buf[2],buf[3], buf[4],buf[5],
-          buf[6],buf[7], buf[8]);
-    } else {
-      PRINTF("Failed to match ROM\n");
-    }
-
-    /* prepare for rom search */
-    onewire_search_rom_setup(&srom, &ow);
-
-    PRINTF("Performing ROM search:\n");
-    if (onewire_search_rom(&srom, &ow)) {
-      PRINTF("ROM search sucessful\n");
-      PRINTF("Family: 0x%X\n", ow.family);
-      PRINTF("ID: 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X\n", ow.id[0],ow.id[1],
-          ow.id[2],ow.id[3], ow.id[4],ow.id[5]);
-      PRINTF("CRC: 0x%X\n", ow.crc);
-
-    } else {
-      PRINTF("ERROR: ROM search failed\n");
-    }
-
     PRINTF("Verifying device is present:\n");
     if (onewire_search_rom_verify(&srom, &ow)) {
       PRINTF("Device present\n");
@@ -164,16 +149,129 @@ PROCESS_THREAD(onewire_test_process, ev, data)
       PRINTF("ERROR: Device missing\n");
     }
 
-    PRINTF("Verifying WRONG device is not present:\n");
-    ow.family = 0x20;
-    if (onewire_search_rom_verify(&srom, &ow)) {
-      PRINTF("ERROR: Wrong device present\n");
-    } else {
-      PRINTF("Device missing\n");
-    }
+    /* Get temperature from commected DS18B20 */
+    if (ow.family == 0x28) {
+      PRINTF("DS18B20 Temperature sensor detected\n");
+      if (onewire_match_rom(&ow)) {
+        PRINTF("MATCH: Starting temp conversion\n");
+        onewire_write_cmd(0x44);
+      } else {
+        PRINTF("Failed to match ROM\n");
+      }
+      if (onewire_match_rom(&ow)) {
+        PRINTF("MATCH: Reading scratch pad:\n");
+        onewire_read_fn_cmd_bytes(0xBE, buf, 9);
+        print_scratch_pad(buf, 9);
 
+        temp = (uint16_t)((uint8_t)buf[0] | ((uint8_t)buf[1] << 8));
+        convert_temp_str(temp, temp_str);
+        PRINTF("Temp: 0x%X = %s C\n", (int16_t)temp, temp_str);
+
+      } else {
+        PRINTF("Failed to match ROM\n");
+      }
+    }
     etimer_reset(&et);
   }
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+static void print_rom_id(struct onewire_dev *ow) {
+  PRINTF("Family: 0x%X\n", ow->family);
+  PRINTF("ID: 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X\n", ow->id[0],ow->id[1],
+      ow->id[2],ow->id[3], ow->id[4],ow->id[5]);
+  PRINTF("CRC: 0x%X\n", ow->crc);
+  return;
+}
+/*---------------------------------------------------------------------------*/
+static void print_scratch_pad(const char *buf, uint8_t count) {
+  int i;
+
+  PRINTF("Scratch pad:\n");
+  for (i = 0; i < count; i++) {
+    PRINTF("0x%X ", (uint8_t)buf[i]);
+  }
+  PRINTF("\n");
+  return;
+}
+/*---------------------------------------------------------------------------*/
+static float convert_temp_float(int16_t temp_2c) {
+
+  int neg = 0;
+  float temp;
+
+  neg = (temp_2c & (1 << 11)) != 0;
+  if (neg) {
+    temp_2c = ~temp_2c + 1;
+  }
+  temp = temp_2c * 0.0625f;
+
+  if (neg) {
+    temp = -temp;
+  }
+
+  return temp;
+}
+/*---------------------------------------------------------------------------*/
+static void convert_temp_str(int16_t temp_2c, char *temp_s) {
+  /* Preferred conversion method since float does not always */
+  /* handle negative precision */
+
+  int neg = 0;
+  int dec;
+  float temp;
+  int i = 0;
+
+  neg = (temp_2c & (1 << 11)) != 0;
+
+  if (neg) {
+    temp_2c = ~temp_2c + 1;
+    temp_s[i++] = '-';
+  }
+  temp = temp_2c * 0.0625f;
+
+  dec = temp;
+  temp = temp - dec;
+  temp_s[sprintf(temp_s, "%d.%04u",
+      dec, (unsigned int)(temp*10000))] = '\0';
+
+  return;
+}
+/*---------------------------------------------------------------------------*/
+static void temp_conversion_tests() {
+  int16_t temp;
+  char temp_str[10];
+
+  temp = 0x07d0;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0x0550;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0x0191;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0x00A2;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0x0008;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0x0000;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0xFFF8;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0xFF5E;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0xFE6F;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+  temp = 0xFC90;
+  convert_temp_str(temp, temp_str);
+  PRINTF("Temp: 0x%X = %s C\n", temp, temp_str);
+
+  return;
+}
